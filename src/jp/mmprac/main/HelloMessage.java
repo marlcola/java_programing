2c64f618e425e5b74698d27e7289734033c8e5aa
mori <mori@mori-PC>
2013-12-16 16:20:33 +0900

    Add HelloMessage class.
    Set a hello message and get this message.

---
+++ src/jp/mmprac/main/HelloMessage.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.mmprac.main;
+
+/**
+ *
+ * @author mori
+ */
+public class HelloMessage {
+    private final String message;
+
+    public HelloMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    
+    
+}

1e3781239bd209eef6c5023e8b1939ae7dd9a2dd
mori <mori@mori-PC>
2013-12-16 13:56:48 +0900

    Change Main.java
    Add hallo message.

--- src/jp/mmprac/main/Main.java
+++ src/jp/mmprac/main/Main.java
@@ -16,6 +16,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!!");
-        
+        System.out.println("Hello Japan!!");
     }
 }
